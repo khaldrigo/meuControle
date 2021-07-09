735f299f34002f57bd0ff85cdd344a9043bd159a
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-07-08 22:03:17 -0300

    Refatoração do componente "item"

--- lib/components/transactions_list.dart
+++ lib/components/transactions_list.dart
@@ -1,7 +1,7 @@
+import 'package:expenses/components/transaction_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -17,14 +17,14 @@ class TransactionList extends StatelessWidget {
               builder: (context, constraints) {
                 return Column(
                   children: [
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
                     Text(
                       "Nenhuma transação cadastrada",
                       style: Theme.of(context).textTheme.headline6,
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
                     Container(
@@ -39,43 +39,9 @@ class TransactionList extends StatelessWidget {
               itemCount: transactions.length,
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('R\$${tr.value.toStringAsFixed(2)}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 400
-                        ? TextButton.icon(
-                            onPressed: () => onRemove(tr.id),
-                            icon: Icon(Icons.delete),
-                            label: Text(
-                              "Excluir",
-                            ),
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => onRemove(tr.id),
-                          ),
-                  ),
+                return TransactionItem(
+                  tr: tr,
+                  onRemove: onRemove,
                 );
               },
             ),
